3e82a7b8c0ca4a57634060dc3f8bb5f0fab17fb4
Rodrigolopezbravo <rodrigolopezbravo6@gmail.com>
2023-04-01 21:01:05 -0300

    Entrega 1

---
+++ main.js
@@ -0,0 +1,21 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('PlantLife')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+
+
+  const btnCart = document.querySelector('.container-icon')
+const containerCartProducts = document.querySelector('.container-cart-products')
+
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart')
+})
